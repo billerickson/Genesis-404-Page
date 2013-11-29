f508988c08251a2de31f93cbda0816f18b60d2eb
GaryJones <gary@garyjones.co.uk>
2013-11-29 11:24:42 +0000

    Fix test class name.

--- tests/test-test.php
+++ tests/test-test.php
@@ -3,7 +3,7 @@
 /**
  * @group genesis-404-page
  */
-class WP_Calendar_Test extends BE_Genesis_404_TestCase {
+class BE_Genesis_404_Test extends BE_Genesis_404_TestCase {
 	function test_test() {
 		$this->assertTrue( true );
 	}
